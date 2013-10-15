6da6479611d671a175b248287a2a44edca9d60c8
Jason Peak <jpeak5@lsu.edu>
2013-10-15 09:14:45 -0500

    tidies lang strings

--- lang/en/local_proctoru.php
+++ lang/en/local_proctoru.php
@@ -1,37 +1,5 @@
 <?php
 $string['mod_name'] = "Proctor U";
-$string['not_registered'] = "You are not registered yet with ProctorU. Please Proceed to the Orientation Course for more information.";
-$string['excluded_courses'] = "Excluded Courses";
-$string['excluded_courses_description'] = "Comma-separated list of course IDs that should not be subject to the restrictions imposed by student orientation";
-
-$string['roleselection'] = 'roleselection';
-$string['roleselection_label'] = 'Roles Exempt';
-$string['roleselection_description'] = 'which roles should be excluded from the PU lookup';
-
-$string['cron_run']  = 'Cron';
-$string['cron_desc'] = 'Run with Cron?';
-
-$string['profilefield_shortname'] = "user_proctoru";
-$string['profilefield_shortname_description'] = "Name of the custom profile field";
-$string['user_proctoru'] = "ProctorU registration status";
-
-$string['proctoru_token'] = 'ProctorU webservice token';
-$string['proctoru_token_description'] = 'token for connecting to ProctorU';
-$string['proctoru_api'] = "ProctorU URL";
-$string['proctoru_api_description'] = "ProctorU API URL";
-
-$string['credentials_location'] = 'Crednetials Location';
-$string['credentials_location_description'] = 'Location of local webservices credentials';
-
-$string['localwebservice_url'] = 'local Webservices URL';
-$string['localwebservice_url_description'] = "URL for the local users' webservice";
-$string['localwebservice_fetchuser_servicename'] = 'User Profile service name';
-$string['localwebservice_fetchuser_servicename_description'] = "Source for user profile information";
-$string['localwebservice_userexists_servicename'] = 'User Profile service name';
-$string['localwebservice_userexists_servicename_description'] = "Source for user profile information";
-
-$string['stu_profile'] = "Student Profile Name";
-$string['stu_profile_description'] = "Student Profile name is a required param to the Webservice query.";
 
 //status codes
 $string['unregistered']      = 'Unregistered';
@@ -41,4 +9,43 @@ $string['exempt']            = 'Exempt';
 $string['sam_profile_error'] = 'SAM ERROR';
 $string['no_idnumber']       = 'NO IDNUMBER';
 $string['pu_404']            = '404 PrU';
+
+// roles to consider exempt
+$string['roleselection'] = 'roleselection';
+$string['roleselection_label'] = 'Roles Exempt';
+$string['roleselection_description'] = 'which roles should be excluded from the PU lookup';
+
+// cron
+$string['cron_run']  = 'Cron';
+$string['cron_desc'] = 'Run with Cron?';
+
+// custom profile field
+$string['profilefield_shortname'] = "user_proctoru";
+$string['profilefield_shortname_description'] = "Name of the custom profile field";
+$string['user_proctoru'] = "ProctorU Registration status";
+
+// ProctorU API details
+$string['proctoru_token'] = 'ProctorU webservice token';
+$string['proctoru_token_description'] = 'token for connecting to ProctorU';
+
+$string['proctoru_api'] = "ProctorU URL";
+$string['proctoru_api_description'] = "ProctorU API URL";
+
+
+// LSU-specific local data store connection settings
+$string['credentials_location'] = 'Crednetials Location';
+$string['credentials_location_description'] = 'Location of local webservices credentials';
+
+// More LSU-specific local data store connection settings
+$string['localwebservice_url'] = 'local Webservices URL';
+$string['localwebservice_url_description'] = "URL for the local users' webservice";
+
+$string['localwebservice_fetchuser_servicename'] = 'User Profile service name';
+$string['localwebservice_fetchuser_servicename_description'] = "Source for user profile information";
+
+$string['localwebservice_userexists_servicename'] = 'User Profile service name';
+$string['localwebservice_userexists_servicename_description'] = "Source for user profile information";
+
+$string['stu_profile'] = "Student Profile Name";
+$string['stu_profile_description'] = "Student Profile name is a required param to the Webservice query.";
 ?>
