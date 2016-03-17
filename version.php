f267a716f3b8cfe68351a100cea5d6528348aada
Robert Russo <rrusso@lsu.edu>
2016-03-17 14:35:17 -0500

    Simple update for 3x

--- version.php
+++ version.php
@@ -1,5 +1,33 @@
 <?php
 
-$plugin->version = 2013102310;
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   local_proctoru
+ * @copyright 2016 Louisiana State University, Jason Peak, Robert Russo
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2016031705;      // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2011070110;      // Requires this Moodle version
+
+$plugin->component = 'local_proctoru';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.1';
 $plugin->cron = 30;
 ?>
